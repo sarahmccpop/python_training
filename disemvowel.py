08626fac260530df2eb7bef30fbb6b817bc4a2f9
Sarah McConville <sarah.mcconville@wegotpop.com>
2021-11-15 12:24:03 +0000

    version 2 using replace

--- disemvowel.py
+++ disemvowel.py
@@ -1,5 +1,5 @@
 #https://www.codewars.com/kata/52fba66badcd10859f00097e/train/python
-
+# version 1 my solution
 myString = 'THIS IS MY STRING'
 myString2 = "My name is Sarah"
 
@@ -12,7 +12,13 @@ def disemvowel(string_):
 
     string_ = "".join(newString) #specify what will be between letters when joined
     return string_
-
-
 print(disemvowel(myString))
 print(disemvowel(myString2))
+
+def disemvowel2(string_):
+    for i in "aeiouAEIOU":
+        string_ = string_.replace(i, "")
+    return string_
+
+print(disemvowel2(myString))
+print(disemvowel2(myString2))
